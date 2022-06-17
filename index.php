a29ae4bf36d3524f9d0c7a71dc9ba6deeca7fdef
KadekDavid23 <davidkurniawan332@gmail.com>
2022-06-17 09:52:50 +0700

    latihan-api-david

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 
 // membutuhkan pemanggilan akses koneksi (mysql)
 // (DISI)
+require 'koneksi.php';
 
 // menjalankan sessions
 // (DISI)
-
+session_start();
 // check apakah session email sudah ada atau belum.
 // jika belum maka akan diredirect ke halaman index (login)
 if (empty($_SESSION['uname'])) {
@@ -95,10 +96,10 @@ if (empty($_SESSION['uname'])) {
                     </li>
                     <br>
                     <li class="nav-item">
-                        <p><strong>NAMA KALIAN</strong></p>
+                        <p><strong>KADEK DAVID KURNIAWAN</strong></p>
                     </li>
                     <li class="nav-item">
-                        <p>NIM KALIAN</p>
+                        <p>6706210126</p>
                     </li>
                     </li>
                 </ul>
